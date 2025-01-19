f7834d3d66fa654b760956973bbde0e9d8611128
naca-nyan <naca.nyan@gmail.com>
2025-01-20 00:09:07 +0900

    Add url base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/sr2-room-viewer/",
   plugins: [deno(), svelte()],
-})
+});
