9037ca651e20ae28eb49e8f4dea4168320008e5d
naca-nyan <naca.nyan@gmail.com>
2025-01-21 00:26:22 +0900

    Add localStorage

--- src/App.svelte
+++ src/App.svelte
@@ -2,56 +2,48 @@
   import { onDestroy } from "svelte";
   import RoomCard from "./lib/RoomCard.svelte";
   import Switch from "./lib/Switch.svelte";
-  import { online } from "./store";
-  const lockValues = ["かぎなし", "かぎあり", "どっちも"] as const;
-  let filterLock = $state<(typeof lockValues)[number]>("かぎなし");
-  const countryValues = ["にほん", "かんこく", "どこでも"] as const;
-  let filterCountry = $state<(typeof countryValues)[number]>("にほん");
-
-  function filter(
-    rooms: Room[],
-    filterLock: (typeof lockValues)[number],
-    filterCountry: (typeof countryValues)[number],
-  ) {
+  import { online, filterLock, filterCountry, autosync } from "./store";
+  import { lockValues, countryValues } from "./store";
+  import type { LockValue, CountryValue } from "./store";
+  function filter(rooms: Room[], l: LockValue, c: CountryValue) {
     return rooms
       .filter((r) => {
-        if (filterLock === "かぎなし") return r.needPasswd === false;
-        if (filterLock === "かぎあり") return r.needPasswd === true;
+        if (l === "かぎなし") return r.needPasswd === false;
+        if (l === "かぎあり") return r.needPasswd === true;
         return true;
       })
       .filter((r) => {
         const provider = r.ownerUser.idProvider;
-        if (filterCountry === "にほん") return provider === "ymid-jp";
-        if (filterCountry === "かんこく") return provider === "ymid-kr";
+        if (c === "にほん") return provider === "ymid-jp";
+        if (c === "かんこく") return provider === "ymid-kr";
         return true;
       });
   }
 
-  let autosync = $state(true);
   const refreshRate = 30 * 1000;
   const interval = setInterval(() => {
-    if (autosync) online.sync();
+    if ($autosync) online.sync();
   }, refreshRate);
   onDestroy(() => clearInterval(interval));
 </script>
 
 <h1>SYNCROOM2 Room Viewer</h1>
 <div class="selector">
-  <select bind:value={filterLock}>
+  <select bind:value={$filterLock}>
     {#each lockValues as v}<option>{v}</option>{/each}
   </select>
-  <select bind:value={filterCountry}>
+  <select bind:value={$filterCountry}>
     {#each countryValues as v}<option>{v}</option>{/each}
   </select>
 </div>
 <div class="autosync">
-  自動更新(30秒) <Switch bind:checked={autosync} />
+  自動更新(30秒) <Switch bind:checked={$autosync} />
 </div>
 <main>
   {#if $online === null}
     <h2>loading...</h2>
   {:else}
-    {#each filter($online.rooms, filterLock, filterCountry) as room (room.roomId)}
+    {#each filter($online.rooms, $filterLock, $filterCountry) as room (room.roomId)}
       <RoomCard {room} />
     {:else}
       <h2>（ルームなし）</h2>
--- src/store.ts
+++ src/store.ts
@@ -17,3 +17,47 @@ function createOnlineStore() {
 }
 
 export const online = createOnlineStore();
+
+function createLocalStorageStore<T>(
+  key: string,
+  defaultValue: T,
+  toString: (v: T) => string,
+  parser: (s: string) => T,
+) {
+  const v = localStorage.getItem(key);
+  const initialValue = v !== null ? parser(v) : defaultValue;
+  const { set, subscribe } = writable<T>(initialValue);
+
+  return {
+    subscribe,
+    set: (v: T) => {
+      localStorage.setItem(key, toString(v));
+      set(v);
+    },
+  };
+}
+export const lockValues = ["かぎなし", "かぎあり", "どっちも"] as const;
+export type LockValue = typeof lockValues[number];
+export const filterLock = createLocalStorageStore<LockValue>(
+  "filterLock",
+  "かぎなし",
+  (v) => v,
+  (s) => lockValues.includes(s as LockValue) ? s as LockValue : "かぎなし",
+);
+
+export const countryValues = ["にほん", "かんこく", "どこでも"] as const;
+export type CountryValue = typeof countryValues[number];
+export const filterCountry = createLocalStorageStore<CountryValue>(
+  "filterCountry",
+  "にほん",
+  (v) => v,
+  (s) =>
+    countryValues.includes(s as CountryValue) ? s as CountryValue : "にほん",
+);
+
+export const autosync = createLocalStorageStore<boolean>(
+  "autosync",
+  true,
+  (v) => v.toString(),
+  (s) => s === "true",
+);
